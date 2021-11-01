ae2c8bb97085a261f749a28dc517050f7b880906
Anuranjan <anuranjanpandey5@gmail.com>
2021-11-01 23:18:52 +0530

    req updated

--- stock.py
+++ stock.py
@@ -1,23 +1,26 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+import datetime
 
 def main():
     st.write("""
     # Stock Price Dashboard
 
     Shown are the stock closing price and volume of stocks!
-    Please select the stock
 
     """)
+    stocks = pd.read_csv('algorithms/sp_500_stocks.csv')
+    stock = st.selectbox('Please select the stock', (stocks['Ticker']))
     #define the ticker symbol
-    tikcerSymbol = 'GOOGL'
+    tikcerSymbol = stock
 
     #get data on this ticker
     tickerData = yf.Ticker(tikcerSymbol)
 
     # get the historical prices for this ticker
-    tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+    cur_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    tickerDf = tickerData.history(period='1d', start='2010-5-31', end=cur_date)
 
     st.line_chart(tickerDf.Close)
     st.line_chart(tickerDf.Volume)
